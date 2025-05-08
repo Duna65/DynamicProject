a8ee8edfc6bd190151a81ffa3e24ba4fbd4ab372
Dallin Lyman <142834133+Duna65@users.noreply.github.com>
2025-05-08 12:22:17 -0600

    Add files via upload

---
+++ src.js
@@ -0,0 +1,8 @@
+function AddText(id,text){
+    const p = document.getElementById(id)
+p.innerText = text
+}
+function ChangeColor(r,g,b){
+document.body.style.backgroundColor = "rgb("+r+","+g+","+b+")";
+}
+
