ca511f27d38db5c2a049dc36e4a2ca5418087591
Dallin Lyman <142834133+Duna65@users.noreply.github.com>
2025-05-08 12:07:42 -0600

    Delete JavaPage directory

--- JavaPage/src.js
+++
@@ -1,8 +0,0 @@
-function AddText(id,text){
-    const p = document.getElementById(id)
-p.innerText = text
-}
-function ChangeColor(r,g,b){
-document.body.style.backgroundColor = "rgb("+r+","+g+","+b+")";
-}
-
